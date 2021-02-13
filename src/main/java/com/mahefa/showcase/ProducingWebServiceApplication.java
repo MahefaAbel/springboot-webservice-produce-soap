2d4416759780f578d6acaad12c242b8c7e20120d
Mahefa Abel <abelmahefa@gmail.com>
2021-02-13 08:57:55 +0300

    Second commmit

--- src/main/java/com/mahefa/showcase/ProducingWebServiceApplication.java
+++ src/main/java/com/mahefa/showcase/ProducingWebServiceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWebserviceProduceSoapApplication {
+public class ProducingWebServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebserviceProduceSoapApplication.class, args);
+		SpringApplication.run(ProducingWebServiceApplication.class, args);
 	}
-
 }
