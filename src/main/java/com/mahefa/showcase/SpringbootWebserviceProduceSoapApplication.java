fdec918f6c4461cb50c3d5fcb58983f5f13a5538
Mahefa Abel <abelmahefa@gmail.com>
2021-02-13 08:34:50 +0300

    Init commit

---
+++ src/main/java/com/mahefa/showcase/SpringbootWebserviceProduceSoapApplication.java
@@ -0,0 +1,13 @@
+package com.mahefa.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceProduceSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebserviceProduceSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahefa/showcase/SpringbootWebserviceProduceSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahefa.showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceProduceSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
